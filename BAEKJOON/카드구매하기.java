dcfac3ed8ce08c3f5461950e406f99b8cd59901f
ssongmi <thdal1232@naver.com>
2021-06-01 20:47:27 +0900

    Update20210601

---
+++ BAEKJOON/카드구매하기.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 카드구매하기 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] price = new int[N+1];
+		int[] dp = new int[N+1];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 1; i < N+1; i++) {
+			price[i] = Integer.parseInt(st.nextToken()); 
+		}
+		
+		for(int i = 1; i <= N; i++ ) {
+			for(int j = 1; j <= i; j++) {
+				dp[i] = Math.max(dp[i], dp[i-j] + price[j]);
+			}
+		}
+		System.out.println(dp[N]);
+	}
+}
