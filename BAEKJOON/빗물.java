df5bb360e0c4362f44a010d9006e7a847ec0b10b
ssongmi <thdal1232@naver.com>
2021-06-28 15:22:09 +0900

    Update20210628

---
+++ BAEKJOON/빗물.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 빗물 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+	public static void main(String[] args) throws Exception{
+		st = new StringTokenizer(br.readLine());
+		int R = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		int prev = -1;
+		for(int i = 0; i < C; i++) {
+			int cur = Integer.parseInt(st.nextToken());
+			
+		}
+	}
+}
